7df49e20b8ee6f84a1305c5cc7236ae2a434b5d4
Juan Pablo Bolanio <juampi360786@gmail.com>
2025-07-27 12:42:28 -0300

    App noticias y usuarios creadas | Modelos realizados: Usuarios, Noticias, Categorías, Imágenes por Noticias

---
+++ apps/noticias/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+from apps.usuarios.models import Usuario
+
+# Create your models here.
+
+class Categoria(models.Model):
+    categoria_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+class Noticia(models.Model):
+    noticia_id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=85)
+    subtitulo = models.CharField(max_length=150)
+    contenido = models.TextField()
+    fecha = models.DateField(auto_now_add=True)
+    categorias = models.ManyToManyField(Categoria)
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+class ImagenNoticia(models.Model):
+    noticia = models.ForeignKey(Noticia, on_delete=models.CASCADE, related_name='imagenes')
+    imagen = models.ImageField(upload_to='noticias/noticias_imagenes')
+
+    def __str__(self):
+        return self.imagen
