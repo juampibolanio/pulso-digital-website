6ec0cc241ff92506450be253be010d74386fb5f7
Juan Pablo Bolanio <juampi360786@gmail.com>
2025-07-28 17:51:28 -0300

    Implementación de Autenticacion: Registro | Login | Logout - Templates base creados

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -2,8 +2,13 @@ app_name = 'apps.usuarios'
 
 from django.urls import path
 
+from apps.usuarios.views import login_view, logout_view, register_view
+
 app_name = "apps.usuarios"
 
 urlpatterns = [
-    
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login' ),
+    path('logout/', logout_view, name='logout'),
+    # path()
 ]
--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from email import message
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from .forms import RegisterForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+# Registro
+def register_view(request):
+    
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('apps.usuarios:login')
+    else:
+        form = RegisterForm()
+
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'auth/registro.html', context )
+
+# Login
+
+def login_view(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('apps.usuarios:register') # Esto luego debe redireccionar a todas las noticias.
+        else:
+            messages.error(request, 'Credenciales incorrectas, vuelva a intentarlo.')
+
+    return render(request, 'auth/login.html', )
+
+
+# Logout
+
+def logout_view(request):
+
+    logout(request)
+
+    return redirect('apps.usuarios:login')
